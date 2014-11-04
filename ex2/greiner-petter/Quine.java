e8cc8fe273e0f440aacbf3fd52bc703009d2ec94
AndreG-P <andre.greiner-petter@t-online.de>
2014-11-04 17:56:23 +0100

    ex3 comes from greiner-petter

---
+++ ex2/greiner-petter/Quine.java
@@ -0,0 +1,1 @@
+class Quine{public static void main(String[] a){String s = "class Quine{public static void main(String[] a){String s = %c%s%c;System.out.printf(s,34,s,34);}}";System.out.printf(s,34,s,34);}}
