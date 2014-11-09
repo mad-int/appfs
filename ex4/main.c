125209c99830125defeb5897cb45299095306128
Leon Eifler <leoneifler@gmail.com>
2014-11-09 12:31:34 +0100

    Initial Commit

---
+++ ex4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ex4
+//
+//  Created by Leon Eifler on 09/11/14.
+//  Copyright (c) 2014 leonoel. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
