cb619fac7f8b5f4b99fc5d7ebc8c2b8795569219
Andre Weltsch <andre.t.weltsch@campus.tu-berlin.de>
2014-11-12 00:14:05 +0100

    Add new testcase for lp parsing.

--- ex4/weltsch/tests/test_linear_program.c
+++ ex4/weltsch/tests/test_linear_program.c
@@ -4,6 +4,7 @@
 #include <cmocka.h>
 
 #include "../src/linear_program.c"
+#define TEST_FILE_1 "test_file_1"
 
 // create an empty
 static void test_lp_creation(void **state) {
@@ -16,9 +17,36 @@ static void test_lp_creation(void **state) {
     lp_free(lp);
 }
 
+static void test_lp_parser(void **state) {
+    int expected_rows = 3;
+    int expected_cols = 3;
+    LinearProgram *lp = new_lp_from_file(TEST_FILE_1);
+    assert_int_equal(lp->rows, expected_rows);
+    assert_int_equal(lp->cols, expected_cols);
+
+    // check matrix entries
+    assert_int_equal(lp->matrix[0][0], 1);
+    assert_int_equal(lp->matrix[0][1], 2);
+    assert_int_equal(lp->matrix[0][2], 3);
+    assert_int_equal(lp->matrix[1][0], 1);
+    assert_int_equal(lp->matrix[1][1], 2);
+    assert_int_equal(lp->matrix[1][2], 3);
+    assert_int_equal(lp->matrix[2][0], -4);
+    assert_int_equal(lp->matrix[2][1], 3);
+    assert_int_equal(lp->matrix[2][2], -5);
+
+    // check vector entries
+    assert_int_equal(lp->vector[0], 3);
+    assert_int_equal(lp->vector[1], 2);
+    assert_int_equal(lp->vector[2], 5);
+
+    print_bin_solutions_lp(lp);
+    lp_free(lp);
+}
+
 int main(void) {
     const UnitTest tests[] = {
-        unit_test(test_lp_creation)
+        unit_test(test_lp_parser),
     };
     return run_tests(tests);
 }
