c58242bc75441e9cafc1ef944bb8b73e9b15e6cf
Andre Weltsch <andre.t.weltsch@campus.tu-berlin.de>
2014-11-12 16:08:37 +0100

    Add possibility for file export of the solutions.

--- ex4/weltsch/src/ex4_main.c
+++ ex4/weltsch/src/ex4_main.c
@@ -10,7 +10,7 @@
  * @return EXIT_FAILURE if file in wrong format
  *         EXIT_SUCCESS if solved
  */
-int solve_lp(const char *filename) {
+int solve_lp(const char *filename, const char* outfile) {
     int rows, cols;
     LinearProgram *lp = new_lp_from_file(filename);
 
@@ -18,7 +18,20 @@ int solve_lp(const char *filename) {
         return EXIT_FAILURE;
     }
 
-    print_bin_solutions_lp(lp);
+    if (strlen(outfile) > 0) {
+        FILE *stream = fopen(outfile, "w");
+
+        if (NULL == stream) {
+            fprintf(stderr, "can't write to outfile %s\n", outfile);
+            return EXIT_FAILURE;
+        }
+
+        fprint_bin_solutions_lp(stream, lp);
+        fclose(stream);
+    } else {
+        print_bin_solutions_lp(lp);
+    }
+
     lp_free(lp);
 
     return EXIT_SUCCESS;
@@ -31,7 +44,12 @@ int main(int argc, char** argv) {
        fprintf(stderr, "alternative usage: %s filename outfile\n", argv[0]);
        return EXIT_FAILURE;
     }
-    // FIXME if argc >= 3 -> fprint_bin_solutions_lp
 
-    return solve_lp(argv[1]);
+    if (argc >= 3 && strlen(argv[2]) > 0) {
+        return solve_lp(argv[1], argv[2]);
+    }
+
+    /* FIXME maybe add a wrapper for this, even though it's not
+     * necessary */
+    return solve_lp(argv[1], "");
 }
