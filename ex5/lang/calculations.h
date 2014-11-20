b9fa4d727537b13bf4e4fb1bb83a7a0edf49c042
Claus Lang <clauslang@yahoo.de>
2014-11-20 18:03:53 +0100

    added solution for ex5

---
+++ ex5/lang/calculations.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   calculations.h
+ * Author: clauslang
+ *
+ * Created on November 17, 2014, 4:44 PM
+ */
+
+#ifndef CALCULATIONS_H
+#define	CALCULATIONS_H
+
+int increment(int* x, int size);
+int scalar_product(int* x, int* y, int size);
+double scalar_product_double(double* x, double* y, int size);
+
+#endif	/* CALCULATIONS_H */
+
+
+
