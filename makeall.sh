99e29fd5b7d727d743aabc1627e33ec41280c14d
Matthias Miltenberger <miltenberger@zib.de>
2014-11-21 10:41:46 +0100

    add coverage tests

--- makeall.sh
+++ makeall.sh
@@ -2,15 +2,31 @@
 #
 # compile the different codes (Makefiles are assumed to be present)
 
+home=`pwd`
+
 ex="ex5"
+
 students=(inken.gamrath simon steger weltsch lang buerschaper schrezenmaier)
 
-home=`pwd`
+tests=($home/data/corrupt* $home/data/error* $home/data/test*)
 
 for student in ${students[@]}
 do
    cd "$ex/$student"
    echo "compiling code for $ex/$student"
+   make clean
    make
+
+   for test in ${tests[@]}
+   do
+      ex5 $test
+   done
+   mkdir cov/
+   # location of compiled code is not known - try some...
+   lcov -d obj/ -c -o cov/coverage.info
+   lcov -d src/ -c -o cov/coverage.info
+   lcov -d . -c -o cov/coverage.info
+   genhtml cov/coverage.info
    cd $home
 done
+
