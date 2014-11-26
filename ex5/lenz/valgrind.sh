165fb6de14028fd29f32de9b6cf130936155d0e6
Ralf Lenz <lenz@zib.de>
2014-11-26 16:20:38 +0100

    add pipe symbol

--- ex5/lenz/valgrind.sh
+++ ex5/lenz/valgrind.sh
@@ -1,5 +1,5 @@
 # valgrind check
-for file in `ls ../../data/* | grep -v "test1d.dat" grep -v "double_test1.dat"`
+for file in `ls ../../data/* | grep -v "test1d.dat" | grep -v "double_test1.dat"`
 do
   echo "valgrind check of file " $file
   valgrind --leak-check=full --show-leak-kinds=all ./ex5 $file
