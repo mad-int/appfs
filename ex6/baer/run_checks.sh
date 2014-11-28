c39f1638a16cdc0f3a303e92de0908883ea09279
Sascha Baer <int@arcor.de>
2014-11-28 01:06:56 +0100

    Advanced Programming - Excerice 6
    submitted by Sascha Baer

---
+++ ex6/baer/run_checks.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+
+# Just cover the usage-lines.
+./ex6 2>&1 &> /dev/null
+
+#
+# Test if parsing is ok in this cases fail.
+#
+for file in `ls ../../data/corrupted_* ../../data/error_* ../../data/double_test1.dat` nofile.dat
+do
+    ./ex6 $file 2>&1 &> /dev/null
+    RETVAL=$?
+
+    if [ $RETVAL -eq 0 ];
+    then
+      echo "Error -> Parsing $file was successful."
+      exit -1
+    fi
+
+    # Comment next line if you don't want positive messages.
+    [ $RETVAL -ne 0 ] && echo "Ok -> Parsing $file failed.";
+done
+
+#
+# Test if parsing is ok in this cases it should be successful.
+#
+for file in `ls ../../data/test?*.dat ../../data/markshare_3_?.dat ../../data/ones16.dat ../../data/test4.dat`
+do
+    ./ex6 $file 2>&1 &> /dev/null
+    RETVAL=$?
+
+    if [ $RETVAL -ne 0 ];
+    then
+      echo "Error -> Parsing $file failed."
+      exit -1
+    fi
+
+    # Comment next line if you don't want positive messages.
+    [ $RETVAL -eq 0 ] && echo "Ok -> Parsing $file was succesful.";
+done
+
+# TODO: Test some good cases and verifiy the solutions.
+# What testing is suppose to do.
+
+echo "All Ok."
+
