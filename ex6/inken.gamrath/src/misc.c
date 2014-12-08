a7e20e355ea23778c90f03344428a5909afd0fac
Inken Gamrath <inken.gamrath@zib.de>
2014-12-08 17:31:30 +0100

    added comments and newlines at end of file

--- ex6/inken.gamrath/src/misc.c
+++ ex6/inken.gamrath/src/misc.c
@@ -4,7 +4,10 @@
 
 #include "misc.h"
 
-TYPE strtov(char* str, char** test)
+TYPE strtov(
+   char* str,         /**< string to be converted */
+   char** test        /**< test string for non-numerical tail of string */
+)
 {
 #if defined INT
    return strtol(str, test, 10);
@@ -13,7 +16,10 @@ TYPE strtov(char* str, char** test)
 #endif /* #if defined INT */
 }
 
-int vtostr(char* str, TYPE value)
+int vtostr(
+   char* str,         /**< string to write value */
+   TYPE value         /**< value to be converted */
+)
 {
 #if defined INT
    sprintf(str, "%i", value);
