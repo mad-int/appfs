f8adc8a1effcc0c120a0f3eafe7321110797c790
Andre Weltsch <andre.t.weltsch@campus.tu-berlin.de>
2014-12-18 00:51:07 +0100

    Clean up array initialization.

--- ex8/weltsch/print_nums.adb
+++ ex8/weltsch/print_nums.adb
@@ -5,36 +5,32 @@ with Ada.Unchecked_Deallocation;
 with Ada.Command_Line;
 
 procedure Print_Nums is
-    type Bool_Arr is array(Natural) of Boolean; -- one entry for each 0 <= int <= Integer'Last
+    -- array related stuff
+    type Bool_Arr is array(Natural) of Boolean with default_component_value => False;
     pragma Pack (Bool_Arr); -- might optimize the array size
     type Bool_Arr_Access is access Bool_Arr; -- pointer type for array, needed to enable heap allocation
     procedure free is new Ada.Unchecked_Deallocation(Bool_Arr, Bool_Arr_Access);
+    Arr : Bool_Arr_Access := new Bool_Arr; -- allocate on heap instead of stack
+
     Input_File : File_Type;
     Input_Stream : Stream_Access;
     Next : Integer;
-    Arr : Bool_Arr_Access := new Bool_Arr; -- allocate on heap instead of stack
 begin
     if Ada.Command_Line.Argument_Count /= 1 then
         Ada.Text_IO.Put_Line("Usage: " & Ada.Command_Line.Command_Name & " <file>");
         Ada.Command_Line.Set_Exit_Status(Ada.Command_Line.Failure);
+        return;
     end if;
 
     Open(Input_File,
          In_File,
-         "nums.dat");
+         Ada.Command_Line.Argument(1));
     Input_Stream := Stream(Input_File);
-
-    -- I would like to initialize like this
-    -- Arr.all := (others => False);
-    -- but this will also allocate a Bool_Arr on the stack...
-    for Num in Natural loop
-        Arr(Num) := False;
-    end loop;
 
     -- go through the file and check the entries in the array
     while not End_Of_File(Input_File) loop
         Integer'Read(Input_Stream, Next);
-        if Next > 0 then
+        if Next >= 0 then
             Arr(Next) := True;
         end if;
     end loop;
